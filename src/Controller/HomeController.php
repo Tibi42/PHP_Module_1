4e9405ed23a6de0a0f0051a8d0fd710aff599761
Tibi42 <guillaume.pecquet@gmail.com>
2025-09-02 14:07:03 +0200

    PHP Symfony

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -17,7 +17,7 @@ final class HomeController extends AbstractController
         $age = 44;
         $sexe = "Masculin";
         $description = "Vous êtes developpeur web en formation";
-        
+        $ami = "Jeanne";
         $tableau_associatif = [
             'ciel' => 'gris',
             'terre' => 'verte',
@@ -26,6 +26,49 @@ final class HomeController extends AbstractController
 
         $tableau_couleur = ['rouge', 'vert', 'bleu'];
 
+        //profil Stagiaire
+        $prenom = "Guillaume";
+        $nom = "Pecquet";
+        $age = 37;
+        $email = "guillaume.pecquet@gmail.com";
+        $telephone = "06 06 06 06 06";
+        $ville = "Paris";
+        $formation = "Developpeur Web";
+        $duree_stage = "18 mois";
+        $competences = ["frontend", "backend", "SEO"];
+        $est_disponible = true;
+        if ($est_disponible) {
+            $est_disponible = "Oui";
+        } else {
+            $est_disponible = "Non";
+        }
+
+        // // Affichage de informations du stagiaire
+        // echo "<H1>Bienvenue " . $prenom . " " . $nom . "<br>je suis heureux de vous revoir</H1>";
+        // echo "<p>Vous avez " . $age . " ans</p>";
+        // echo "<h2>Votre email est : " . $email . "</h2>";
+        // echo "<p>Votre telephone est : " . $telephone . "</p>";
+        // echo "<ul>
+        //     <li>Votre ville est : " . $ville . "</li>
+        //     <li>Votre formation est : " . $formation . "</li>
+        //     <li>Votre duree de stage est : " . $duree_stage . "</li>
+        // </ul>";
+        // echo "<p>Vos competences sont : " . implode(", ", $competences) . "</p>";
+        // echo "<p>Vous etes disponible : " . $est_disponible . "</p>";
+
+        $tableau_associatif = [
+            'Nom' => $nom,
+            'Prenom' => $prenom,
+            'Age' => $age,
+            'Email' => $email,
+            'Telephone' => $telephone,
+            'Ville' => $ville,
+            'Formation' => $formation,
+            'Duree_stage' => $duree_stage,
+            'Competences' => $competences,
+            'Est_disponible' => $est_disponible,
+        ];
+       
         // dd($nom, $age, $sexe, $description); test de variable
         return $this->render('home/index.html.twig', [
             'stagiaire_name' => $prenom,
@@ -33,8 +76,11 @@ final class HomeController extends AbstractController
             'stagiaire_age' => $age,
             'stagiaire_sexe' => $sexe,
             'stagiaire_description' => $description,
-            'hobby' => $tableau_associatif,
-            'couleur' => $tableau_couleur[1],
+            'tableau' => $tableau_associatif,
+            'couleur' => $tableau_couleur,
+            'tableau_associatif' => $tableau_associatif,
+            'ami' => $ami,
         ]);
+        
     }
 }
